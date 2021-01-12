bb34ff355b66cabd6c81bbc3497fe8a4f79fa36e
Kerem <kerem0comert@gmail.com>
2021-01-13 00:50:46 +0300

    Need to clarify backtracking algorithm in my mind

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ As such, I keep the end point square for simplicity as well. By keeping this ext
 to calculate for obstacles each time. Similarly, it also makes sense to keep the length of WordPlacer
 at compile time statically."""
 placers = [
-    WordPlacer(1,Square(0,0),Square(0,4)),
-    WordPlacer(2,Square(0,2),Square(4,2)),
-    WordPlacer(3,Square(0,4),Square(4,4)),
-    WordPlacer(4,Square(2,1),Square(2,4)),
-    WordPlacer(5,Square(2,3),Square(5,3)),
-    WordPlacer(6,Square(3,0),Square(5,0)),
-    WordPlacer(7,Square(3,2),Square(3,4)),
-    WordPlacer(8,Square(4,0),Square(4,4))
+    WordPlacer(0,Square(0,0),Square(0,4)),
+    WordPlacer(1,Square(0,2),Square(4,2)),
+    WordPlacer(2,Square(0,4),Square(4,4)),
+    WordPlacer(3,Square(2,1),Square(2,4)),
+    WordPlacer(4,Square(2,3),Square(5,3)),
+    WordPlacer(5,Square(3,0),Square(5,0)),
+    WordPlacer(6,Square(3,2),Square(3,4)),
+    WordPlacer(7,Square(4,0),Square(4,4))
     ]
 
 """It should be better to start from the WordPlacers with higher length, so that the roots of the 
@@ -41,7 +41,8 @@ CSP Tree are constructed from longer WordPlacers. Intiutively I thought that hav
 at the roots will result in more backtracking work to be done, however this idea of mine might be
 irrelevant. In any case I decided to sort the WordPlacers and start with the ones that have the 
 highest length."""
-placers.sort(key=lambda p: p.length, reverse=True)
+#placers.sort(key=lambda p: p.length, reverse=True)
+#for p in placers: print(p.id)
 
 board = Board(layout, wordList, placers)
 #print(board.placersList[6].length)
@@ -62,31 +63,36 @@ def placeWord(wordPlacer, word):
     return False
 
 
-for wordPlacer in board.placersList:
-    print(f"Trying {wordPlacer.id}")
+currentPos = 0
+while currentPos < len(board.placersList):
+    currentPlacer = board.placersList[currentPos]
+    print(f"Trying {currentPlacer.id}")
     isPlaced = False
-    for word in board.getWordsOfLength(wordPlacer.length, []): #for all words of length = wordPlacer.length    
-        if placeWord(wordPlacer, word):
+    for word in board.getWordsOfLength(currentPlacer.length, []): #for all words of length = wordPlacer.length    
+        if placeWord(currentPlacer, word):
             isPlaced = True
+            currentPos = currentPlacer.id + 1
             break
             
-        
     while not isPlaced: #none of the remaining words could be placed, need backtracking.
         excludedWords = []
-        print(f"Nothing could be placed to placer: {wordPlacer.id}")
+        print(f"Nothing could be placed to placer: {currentPlacer.id}")
         for p in board.filledPlacers: print(p.id, end = ",")
         print("\n")
         lastWordPlacer = board.filledPlacers.pop() #get the last filled WordPlacer
-        print(f"Backtracking to {lastWordPlacer.id} with word {lastWordPlacer.word}")
+        currentPos -= 1
+        print(f"Backtracking to {lastWordPlacer.id} that had {lastWordPlacer.word}")
         excludedWords.append(lastWordPlacer.word)
         lastWordPlacer.word = None
         board.clearWordPlacer(lastWordPlacer)
         board.printBoard()
-        for word in board.getWordsOfLength(lastWordPlacer.length, excludedWords):
-            if placeWord(wordPlacer, word): 
+        for word in board.getWordsOfLength(lastWordPlacer.length, excludedWords):      
+            if placeWord(lastWordPlacer, word): 
                 isPlaced = True
+                currentPos = lastWordPlacer.id + 1
                 break
         #board.printBoard()"""
+   
     
 
 
